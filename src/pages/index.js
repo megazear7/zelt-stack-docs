4b102b48f435242c8cc7f5439a70a7fd9ca1781b
Alex Lockhart <alexlockhart7@protonmail.com>
2026-01-14 05:41:26 -0500

    Style and content updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,4 @@ import { html } from 'orison';
 
 export default context => html`
   <section>${context.mdFile('./src/partials/getting-started.md')}</section>
-  <section>
-    <h3>Here are some example pages</h3>
-  </section>
 `;
